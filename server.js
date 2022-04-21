024acd443255413f43d5f4269a45a96c7d37827a
Brady Mavetz <b.mavetz@outlook.com>
2022-04-20 23:53:58 -0500

    added link to notes.html through server.js

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 // const apiRoutes = require('./routes/apiRoutes');
 // const htmlRoutes = require('./routes/htmlRoutes');
 const app = express();
@@ -6,4 +7,8 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
+
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
 app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
