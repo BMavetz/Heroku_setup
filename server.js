0bbf2b795621ff0ec61bbfb2829cb1374b4874ca
Brady Mavetz <b.mavetz@outlook.com>
2022-04-21 22:47:27 -0500

    setup file path in server.js

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const path = require('path');
-// const apiRoutes = require('./routes/apiRoutes');
-// const htmlRoutes = require('./routes/htmlRoutes');
+const api = require('./routes/index');
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use('/api', api);
 app.use(express.static('public'));
+
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
 
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
